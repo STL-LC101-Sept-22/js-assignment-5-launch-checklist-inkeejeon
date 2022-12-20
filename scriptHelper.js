778b0b406611ff8396567090bcbe5db06d7c9860
Inkee Jeon <inkeejeon@gmail.com>
2022-12-19 18:40:08 -0600

    Progress

--- scriptHelper.js
+++ scriptHelper.js
@@ -3,38 +3,85 @@ require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
-   /*
+   let missionTarget = document.getElementById("missionTarget");
+   missionTarget.innerHTML = `
                 <h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
+                    <li>Name: ${name}</li>
+                    <li>Diameter: ${diameter}</li>
                     <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
+                    <li>Distance from Earth: ${distance}</li>
+                    <li>Number of Moons: ${moons}</li>
                 </ol>
-                <img src="">
-   */
+                <img src="${imageUrl}">
+                `;
 }
 
 function validateInput(testInput) {
-   
+   if (testInput === '') {
+    return "Empty";
+   } else if (isNaN(testInput)) {
+    return "Not a Number";
+   } else {
+    return "Is a Number";
+   }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+   let pilotStatus = document.getElementById("pilotStatus");
+   let copilotStatus = document.getElementById("copilotStatus");
+   let fuelStatus = document.getElementById("fuelStatus");
+   let cargoStatus = document.getElementById("cargoStatus");
    
-}
+
+   if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
+    alert("All fields are required!");
+   } else if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number" || validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
+    alert("Please enter valid information");
+   } else {
+   //use template literal
+   list.style.visibility = "visible";
+   pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
+   copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
+   let launchStatus = document.getElementById("launchStatus");
+
+   if (fuelLevel < 10000 && cargoLevel >= 10000) {
+    fuelStatus.innerHTML = "Fuel level too low for launch";
+    cargoStatus.innerHTML = "Cargo mass too heavy for launch";
+    launchStatus.innerHTML = "Shuttle not ready for launch";
+    launchStatus.style.color = "rgb(199, 37, 78)";
+   } else if (fuelLevel < 10000 && cargoLevel < 10000) {
+    fuelStatus.innerHTML = "Fuel level too low for launch";
+    cargoStatus.innerHTML = "Cargo Mass low enough for launch";
+    launchStatus.innerHTML = "Shuttle not ready for launch";
+    launchStatus.style.color = "rgb(199, 37, 78)";
+    } else if (fuelLevel >= 10000 && cargoLevel >= 10000){
+    fuelStatus.innerHTML = "Fuel level high enough for launch";
+    cargoStatus.innerHTML = "Cargo Mass too heavy for launch";
+    launchStatus.innerHTML = "Shuttle not ready for launch";
+    launchStatus.style.color = "rgb(199, 37, 78)";
+    } else {
+    fuelStatus.innerHTML = "Fuel level high enough for launch";
+    cargoStatus.innerHTML = "Cargo Mass low enough for launch";
+    launchStatus.innerHTML = "Shuttle is ready for launch!";
+    launchStatus.style.color = 'rgb(65, 159, 106)';
+    }
+   }
+ };
 
 async function myFetch() {
     let planetsReturned;
-
-    planetsReturned = await fetch().then( function(response) {
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+        return response.json();
         });
 
     return planetsReturned;
-}
+};
 
 function pickPlanet(planets) {
-}
+let planet = Math.floor(math.random() * planets.length);
+return planets[planet];
+};
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
